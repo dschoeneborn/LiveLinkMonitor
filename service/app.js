380f0890320390dc81bf9da292bc161c9bf8b7e8
Administrator <mail@dennis-schoeneborn.de>
2016-07-28 15:11:37 +0200

    Router and package * implements the main route /api * added package.json

--- service/app.js
+++ service/app.js
@@ -5,11 +5,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+var router = express.Router();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var routes = require("./routes/routes.js")(app);
+var routes = require("./routes/routes.js")(router);
 
+app.use('/api', router);
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
